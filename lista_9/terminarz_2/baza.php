494725bd6f858b0a9a695f60b992fe9ae2df9d62
TypowyJugol <wojtek.roman254@gmail.com>
2020-02-10 21:02:23 +0100

    l8

--- lista_9/terminarz_2/baza.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$baza=new SQLite3('baza.db');
-	$baza->query("create table if not exists terminarz
-		(id integer primary key autoincrement,
-		poczatek char(50),
-		czas char(80),
-		nazwa char(30),
-		 text
-		)");
--- lista_9/terminarz_2/login.php
+++
@@ -1,5 +0,0 @@
-<form method=post action='login_commit.php'>
-<input name=login>
-<input name=password type=password>
-<input type=submit>
-</form>
